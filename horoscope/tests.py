5ff22ef1c43840ddfe97d6901b00691b0c37ed4a
Evgen Bratenkov <giertrut@mail.ru>
2023-12-28 12:03:55 +0300

    added newTests

--- horoscope/tests.py
+++ horoscope/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from horoscope.views import zodiac_dict
+from django.urls import reverse
 
 
 # Create your tests here.
@@ -10,14 +11,16 @@ class TestHoroscope(TestCase):
         response = self.client.get('/horoscope/')
         self.assertEqual(response.status_code, 200)
 
-    def test_libra(self):
+    def test_signs(self):
         sign_zodiac_list = list(zodiac_dict)
         for sign in sign_zodiac_list:
-            response = self.client.get(f'/horoscope/{sign}/')
+            redirect_url = reverse('horoscope-name', args=[sign])
+            response = self.client.get(redirect_url)
             self.assertEqual(response.status_code, 200)
             self.assertIn(zodiac_dict.get(sign), response.content.decode())
 
-    def test_libra_redirect(self):
-        response = self.client.get('/horoscope/7/')
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/horoscope/libra/')
+    def test_sign_redirect(self):
+        for i in range(1, len(zodiac_dict)+1):
+            response = self.client.get(f'/horoscope/{i}/')
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(response.url, f'/horoscope/{list(zodiac_dict)[i-1]}/')
