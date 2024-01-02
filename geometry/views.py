c0d5d3b6e8e3b15b8c14257d1c29e3a5fe32a29b
Evgen Bratenkov <giertrut@mail.ru>
2024-01-02 10:30:50 +0300

    added template Geometry

--- geometry/views.py
+++ geometry/views.py
@@ -1,32 +1,39 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from math import pi
 from django.urls import reverse
+from django.shortcuts import render
+from math import pi
 
 
 # Create your views here.
 
 def get_rectangle_area(request, width, height):
-    return HttpResponse(f'Площадь прямоугольника размером {width}x{height} равна {width * height}')
+    S = width * height
+    # return HttpResponse(f'Площадь прямоугольника размером {width}x{height} равна {S}')
+    return render(request, 'geometry/rectangle.html')
 
 
 def get_square_area(request, width):
-    return HttpResponse(f'Площадь квадрата размером {width}x{width} равна {width * width}')
+    S = width * width
+    # return HttpResponse(f'Площадь квадрата размером {width}x{width} равна {S}')
+    return render(request, 'geometry/square.html')
 
 
 def get_circle_area(request, radius):
-    return HttpResponse(f'Площадь круга радиуса {radius} равна {int(pi * radius ** 2)}')
+    D = pi * radius ** 2
+    # return HttpResponse(f'Площадь круга радиуса {radius} равна {D}')
+    return render(request, 'geometry/circle.html')
 
 
 def rectangle(request, width, height):
-    rectangle_url = reverse('rectangle-name', args=(width, height, ))
+    rectangle_url = reverse('rectangle-name', args=(width, height,))
     return HttpResponseRedirect(rectangle_url)
 
 
 def square(request, width):
-    square_url = reverse('square-name', args=(width, ))
+    square_url = reverse('square-name', args=(width,))
     return HttpResponseRedirect(square_url)
 
 
 def circle(request, radius):
-    circle_url = reverse('circle-name', args=(radius, ))
+    circle_url = reverse('circle-name', args=(radius,))
     return HttpResponseRedirect(circle_url)
